a02ce15348c1e15e414f56cddaf08fe684673c4c
Kacie Donovan <153126726+kend24@users.noreply.github.com>
2025-09-11 11:59:50 +0100

    remove unwanted  menu and related styling & js

--- src/main.js
+++ src/main.js
@@ -25,27 +25,3 @@ document.addEventListener("DOMContentLoaded", () => {
 window.addEventListener("load", () => {
   document.body.classList.remove("preload");
 });
-
-const tabs = document.querySelectorAll("[role=tab]");
-const sections = document.querySelectorAll("[role=tabpanel]");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((tab) => tab.setAttribute("aria-selected", "false"));
-
-    tab.setAttribute("aria-selected", "true");
-    tabs.forEach((t) => t.classList.remove("active"));
-    tab.classList.add("active");
-
-    sections.forEach((section) => {
-      section.setAttribute("aria-hidden", "true");
-      section.classList.add("inactive-section");
-      section.classList.remove("active-section");
-    });
-
-    const targetSection = document.getElementById(tab.dataset.target);
-    targetSection.setAttribute("aria-hidden", "false");
-    targetSection.classList.add("active-section");
-    targetSection.classList.remove("inactive-section");
-  });
-});
